490e83f7b0277eaf101c5c512ba34fe3ed6d4635
Guido Perdomo <guidop91@gmail.com>
2020-10-20 15:16:15 -0400

    Add API stuff and secret managing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+const APP_ID = process.env.REACT_APP_ID;
+const API_KEY = process.env.REACT_APP_KEY;
+const apiUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+
+const App = () => {
+  useEffect(() => {
+    getRecipes();
+  });
+
+  async function getRecipes() {
+    const response = await fetch(apiUrl);
+    const body = await response.json();
+    console.log(body);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipes!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" placeholder="Enter a term" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
